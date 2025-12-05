6d24146c84a0cd2e136d519a3b0ea6cc53b86175
Phoovich <Phoovich@gmail.com>
2025-12-05 13:44:11 +0700

    airflow: add fetch and concat row dags

---
+++ dags/traffy_api_dag.py
@@ -0,0 +1,23 @@
+from airflow.decorators import dag, task  # type: ignore
+from pendulum import datetime
+
+# import ฟังก์ชันจากไฟล์ jobs/traffy_fetch.py
+from jobs import fetch_traffy_and_save_raw
+
+
+@task
+def call_api_and_save():
+    # แค่เรียกฟังก์ชันที่เราเขียนไว้
+    return fetch_traffy_and_save_raw()
+
+@dag(
+    dag_id="traffy_api_every_10min",
+    schedule="*/10 * * * *",   # ทุก 10 นาที
+    start_date=datetime(2025, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["call_api", "traffy"],
+)
+def api_dag():
+    call_api_and_save()
+
+api_dag()
---
+++ dags/traffy_concat_dag.py
@@ -0,0 +1,24 @@
+from airflow.sdk import dag, task  # type: ignore
+
+# import ฟังก์ชันจาก jobs/
+from jobs import concat_traffy_raw_to_processed
+from pendulum import datetime
+
+
+@task
+def concat_task():
+    return concat_traffy_raw_to_processed()
+
+
+@dag(
+    dag_id="traffy_concat_every_30min",
+    schedule="*/30 * * * *",  # ← ทุก 30 นาที
+    start_date=datetime(2025, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["traffy", "concat"],
+)
+def concat_dag():
+    concat_task()
+
+
+concat_dag()
