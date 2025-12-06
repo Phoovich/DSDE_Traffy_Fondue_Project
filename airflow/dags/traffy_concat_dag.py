89a5cdca90555429b389eb0ed54815b0c2feed6c
Phoovich <Phoovich@gmail.com>
2025-12-06 19:26:02 +0700

    airflow: change time to run dags

--- airflow/dags/traffy_concat_dag.py
+++ airflow/dags/traffy_concat_dag.py
@@ -1,24 +1,19 @@
 from airflow.sdk import dag, task  # type: ignore
-
-# import ฟังก์ชันจาก jobs/
 from jobs.traffy_concat import concat_traffy_raw_to_processed
 from pendulum import datetime
-
 
 @task
 def concat_task():
     return concat_traffy_raw_to_processed()
 
-
 @dag(
-    dag_id="traffy_concat_every_30min",
-    schedule="*/30 * * * *",  # ← ทุก 30 นาที
+    dag_id="traffy_concat_daily_1am",
+    schedule="0 1 * * *",  # run 01:00 UTC every day
     start_date=datetime(2025, 1, 1, tz="UTC"),
     catchup=False,
     tags=["traffy", "concat"],
 )
 def concat_dag():
     concat_task()
-
 
 concat_dag()
